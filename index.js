13c9818af29a2b2be9c49359ceaee5db78b071e5
DiegoQuesadaDEV <110751513+DiegoQuesadaDEV@users.noreply.github.com>
2022-08-16 13:09:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let miNombre = "Diego";
+let miEdad = 25;
+let soyDesarrollador = true;
+let fechaNacimiento = new Date(1997, 6, 10);
+const miLibroFavorito = {
+    titulo: "1984",
+    autor: "George Orwell",
+    fecha: new Date(1949, 5, 8),
+    url: "https://es.wikipedia.org/wiki/1984_(novela)"
+};
